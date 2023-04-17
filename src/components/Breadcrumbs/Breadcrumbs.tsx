dd2f8e81404e350729857bce40135c33aa0a7b14
Domagoj <paulic.domagoj@gmail.com>
2023-04-17 06:19:15 +0200

    update breadcrumbs component style

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -16,12 +16,15 @@ export function Breadcrumbs({ children }: BreadcrumbsProps) {
 type CrumbProps = {
   href: string;
   children: ReactNode;
+  isCurrentPage?: boolean;
 };
 
-export function Crumb({ href, children }: CrumbProps) {
+export function Crumb({ href, isCurrentPage, children }: CrumbProps) {
   return (
     <CrumbWrapper>
-      <CrumbLink href={href}>{children}</CrumbLink>
+      <CrumbLink href={href} aria-current={isCurrentPage ? 'page' : undefined}>
+        {children}
+      </CrumbLink>
     </CrumbWrapper>
   );
 }
@@ -34,15 +37,19 @@ const BreadcrumbList = styled.ol`
 
 const CrumbWrapper = styled.li`
   display: inline;
-  --spacing: 8px;
+  --spacing: 12px;
 
   &:not(:first-of-type) {
     margin-left: var(--spacing);
 
     &:before {
-      content: '/';
+      content: '';
+      display: inline-block;
+      border-right: 1px solid white;
+      transform: rotate(15deg);
       opacity: 0.25;
       margin-right: var(--spacing);
+      height: 0.8em;
     }
   }
 `;
